bc2ab3a11e576c21a2e953a94151c91fe990dc14
aski <aski1@mail.ru>
2024-03-15 09:18:17 +0300

    v 0.1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Edu
+{
+    class Program
+    {
+        public static void Main() {
+            Matrix m1 = new Matrix(3, 4);
+            m1.FillRandomValues();
+            Console.WriteLine(m1);
+
+            Matrix m2 = Matrix.GetRandomMatrix(4, 4);
+            Console.WriteLine(m2);
+
+            Matrix res = null;
+            try { 
+                res = m1 + m2;
+            }
+            catch(Exception ex) {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            Console.WriteLine(res.Items[0,0]);
+
+            
+
+            Console.WriteLine("!!!");
+
+            // Matrix m2 = Matrix.GetRandomMatrix(3, 4);
+            // Console.WriteLine(m2);
+        }
+
+    }
+}
