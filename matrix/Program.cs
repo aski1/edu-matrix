0521b4d2df9e2c0125785c294977234c896b0cfc
Evelina Soldatova <136226534+Eve2011@users.noreply.github.com>
2024-04-04 20:15:27 +0300

    Add files via upload

---
+++ matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Edu
+{
+    class Program
+    {
+        public static void Main() {
+            Matrix m1 = new Matrix(3, 4);
+            m1.FillRandomValues();
+            
+            Console.WriteLine("m1:");
+            Console.WriteLine(m1);
+
+            Matrix m2 = Matrix.GetRandomMatrix(4, 4);
+            Console.WriteLine("m2:");
+            Console.WriteLine(m2);
+
+            Matrix res = m1 + m2;
+            Console.WriteLine("Сумма матриц: (если не указана, значит размеры матриц не равны)");
+            Console.WriteLine(res);
+
+            res = -m1;
+            Console.WriteLine("Унарный минус для m1:");
+            Console.WriteLine(res);
+
+            res = m1 - m2;
+            Console.WriteLine("Разность матриц: (если не указана, значит размеры матриц не равны)");
+            Console.WriteLine(res);
+            
+            res = m1 * m2;
+            Console.WriteLine("Произведение матриц: (если не указана, значит размеры матриц не равны)");
+            Console.WriteLine(res);
+        }
+
+    }
+}
